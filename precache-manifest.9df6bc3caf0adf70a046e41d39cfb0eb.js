08f3cfcf8289d819ac6ab645443212626cd560f0
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-21 11:00:23 +0000

    Updates

---
+++ precache-manifest.9df6bc3caf0adf70a046e41d39cfb0eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cec70c7c4bd871e9bdb2a62da56e83",
+    "url": "/react-hotels/index.html"
+  },
+  {
+    "revision": "123f5a27bff0fc91fbb9",
+    "url": "/react-hotels/static/css/main.c4c2a09c.chunk.css"
+  },
+  {
+    "revision": "ebb42edccaa91f56a6b3",
+    "url": "/react-hotels/static/js/2.1c64fb2d.chunk.js"
+  },
+  {
+    "revision": "123f5a27bff0fc91fbb9",
+    "url": "/react-hotels/static/js/main.125321b5.chunk.js"
+  },
+  {
+    "revision": "84c13d5131a91a49f037",
+    "url": "/react-hotels/static/js/runtime-main.8f2aa0d4.js"
+  },
+  {
+    "revision": "365ea9ee4f651af3d13ae9d60000e9a1",
+    "url": "/react-hotels/static/media/hotelBoring.365ea9ee.jpg"
+  },
+  {
+    "revision": "ab3d15d9ad1ce35cb8baf5e039f8861d",
+    "url": "/react-hotels/static/media/hotelWarm.ab3d15d9.jpg"
+  },
+  {
+    "revision": "7213f862be849910dfdd033cb6cb06d2",
+    "url": "/react-hotels/static/media/oskar.7213f862.jpg"
+  },
+  {
+    "revision": "9f38936381ab0eda2ea56e7622cc6757",
+    "url": "/react-hotels/static/media/terry.9f389363.jpg"
+  }
+]);
